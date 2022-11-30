95339ddbe28959b7dee41daee2cba498836793f0
Dev Varma <devtronvarma@gmail.com>
2022-11-30 11:04:13 -0600

    add logic to day-checker class-weekend-boring hw

--- class-12/class-weekend-boring/js/main.js
+++ class-12/class-weekend-boring/js/main.js
@@ -1,10 +1,22 @@
 document.querySelector('#check').addEventListener('click', check)
 
+const output = document.querySelector('#placeToSee')
+
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toUpperCase()
+
+  let result = '';
 
   //Conditionals go here
+  if (day === 'TUESDAY' || day === 'THURSDAY') {
+    result = 'Class!'
+  } else if (day === 'SATURDAY' || day === 'SUNDAY') {
+    result = 'Weekend!'
+  } else {
+    result = 'Yawn! Boring.'
+  }
 
+  output.innerHTML = result;
 
 }
