77e6c2b638bc69ac76c30eee044cb62d6cb1bc50
Dev Varma <devtronvarma@gmail.com>
2022-12-01 10:51:50 -0600

    complete temp-converter exercise in-class

--- class-13/temperature-converter/js/main.js
+++ class-13/temperature-converter/js/main.js
@@ -1,1 +1,16 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+// Get a smurf to listen for a button click
+document.querySelector('#submit').addEventListener('click', convert)
+
+
+function convert() {
+    // Need to value that is in celsius
+    const celsius = document.querySelector('#celsius').value
+
+    // When the button clicks, do the math to convert
+    const convertedTemp = (celsius * 9 / 5) + 32
+
+    // Output the converted number to the DOM
+    document.querySelector('#output').innerHTML = convertedTemp;
+}
