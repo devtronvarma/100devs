82389bf1d8d0eabe119fccfc7a7dc442b0652782
Dev Varma <devtronvarma@gmail.com>
2022-12-02 09:33:25 -0600

    rewrite day-checker with switch-case syntax

--- class-14/class-weekend-boring/js/main.js
+++ class-14/class-weekend-boring/js/main.js
@@ -4,14 +4,39 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase()
 
-  if(day === "tuesday" || day === "thursday"){
-    console.log("YOU HAVE CLASS")
-  }else if( day === "saturday" || day === "sunday"){
-    console.log("Its The Weekend")
-  }else{
-    console.log("BORING")
+  const daysOfWeek = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday'
+  ]
+
+  let output = document.querySelector('#placeToSee')
+
+  if (daysOfWeek.includes(day)) {
+    switch (day) {
+      case 'tuesday' || 'thursday':
+        output.innerText = `It's class day!`
+        break;
+
+      case 'saturday' || 'sunday':
+        output.innerText = `It's the weekend!`;
+        break;
+
+      case 'wednesday':
+        output.innerText = `Woof. Hump day.`
+        break;
+
+      default:
+        output.innerText = `Just another boring day.`
+
+    }
+  } else {
+    alert(`That's not a valid input.`)
   }
-
 }
