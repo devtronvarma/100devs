e3857754cd60fa4bbef5cb92a64d4109d99d672a
Dev Varma <devtronvarma@gmail.com>
2022-12-03 21:37:57 -0600

    rewrite navbar for mobile responsivenes

--- layout-practice/level-ground-layout/js/main.js
+++ layout-practice/level-ground-layout/js/main.js
@@ -1,1 +1,21 @@
 
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+// Toggle mobile menu
+function toggleNavMenu() {
+
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+
+        // add hamburger button
+        toggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    } else {
+        menu.classList.add('active');
+
+        toggle.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    }
+}
+
+// Event Listener for navbar menu toggle
+toggle.addEventListener('click', toggleNavMenu, false);
