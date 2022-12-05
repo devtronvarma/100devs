a1e98e7ef36d4e212b664ab68290c66ecbe62863
Dev Varma <devtronvarma@gmail.com>
2022-12-05 10:57:43 -0600

    complete simple functions exercise in-class

--- class-16/simple-functions/js/main.js
+++ class-16/simple-functions/js/main.js
@@ -1,12 +1,31 @@
 //---Easy
 //create a function that subtracts two numbers and alerts the difference
-
+function difference(numA, numB) {
+    alert(numA - numB);
+}
 //create a function that divides three numbers and console logs the quotient
-
+function divideThree(numA, numB, numC) {
+    console.log(numA / numB / numC);
+}
 //create a function that multiplys three numbers and returns the product
-
+function multiplyThree(numA, numB, numC) {
+    return numA * numB * numC;
+}
 //---Medium
 //create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
-
+function addTwoDivideThird(numA, numB, numC) {
+    return (numA + numB) % numC;
+}
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+function multiplyTwoAndThenSome(numA, numB, numC, numD) {
+    const product = numA * numB;
+    if (product > 100) {
+        console.log(product += numC + numD);
+    } else if (product < 100) {
+        console.log(product -= numC - numD);
+    } else {
+        product *= numC;
+        alert(product % numD);
+    }
+}
