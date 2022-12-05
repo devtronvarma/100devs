0dba7f20908c26a5775e8ff37c90064fa595bfdb
Dev Varma <devtronvarma@gmail.com>
2022-12-05 08:41:41 -0600

    complete age-checker js in class 16

--- class-16/age-checker/js/main.js
+++ class-16/age-checker/js/main.js
@@ -5,9 +5,31 @@
 //If under 25, tell them they can not rent cars affordably
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
+const age = document.querySelector('#danceDanceRevolution').value
 
+function checkAge() {
+    let result;
+
+    if (age < 16) {
+        result = `You cannot drive.`;
+    } else if (age < 18) {
+        result = `You can't hate from outside the club -- you can't even get in.`;
+    } else if (age < 21) {
+        result = `You cannot drink.`;
+    } else if (age < 25) {
+        result = `You cannot rent cars affordably.`;
+    } else if (age < 30) {
+        result = `You cannot rent fancy cars affordably.`;
+    } else {
+        result = `There's nothing left to look forward to.`
+    }
+
+    document.querySelector('#resultOutput').innerText = result;
+}
 
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+document.querySelector('h1').addEventListener('click', checkAge);
