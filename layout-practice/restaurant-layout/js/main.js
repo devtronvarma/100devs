0170cbf528ec5dad71609844a7f03347cfd2d763
Dev Varma <devtronvarma@gmail.com>
2022-12-08 09:30:20 -0600

    add mobile responsive navbar

--- layout-practice/restaurant-layout/js/main.js
+++ layout-practice/restaurant-layout/js/main.js
@@ -1,1 +1,25 @@
+// Mobile responsive navigation bar
 
+// Name toggle and menu items for mobile responsive design
+const toggle = document.querySelector('.toggle')
+const menu = document.querySelector('.menu')
+
+// Function to toggle menu items
+function toggleMenuItems() {
+    // If menu has active class, remove it
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active');
+
+        // Change toggle visual to hamburger button
+        toggle.innerHTML = `<a href='#'><i class="fa-solid fa-bars"></i></a>`;
+    } else {
+        // Otherwise, add that class to it
+        menu.classList.add('active');
+
+        // Change toggle visual to x
+        toggle.innerHTML = `<a href='#'><i class="fa-solid fa-xmark"></i></a>`;
+    }
+
+}
+
+toggle.addEventListener('click', toggleMenuItems)
