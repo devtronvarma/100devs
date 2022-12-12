d2a38aff2061e32022b5c9fb5fda67b02aeb3b38
Dev Varma <devtronvarma@gmail.com>
2022-12-12 09:59:37 -0600

    complete arrays exercises

--- class-19/arrays-fight-to-the-finish/js/main.js
+++ class-19/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,18 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
+let movies = [
+    'Bring It On',
+    'Bring It On Again',
+    'It Has Already Been Brought'
+]
 
+movies.forEach((x) => document.querySelector('h2').innerText += x);
 
 //Create an array of numbers. Loop through the array and three to each number and replace the old number.
+let nums = [10, 20, 30]
 
+nums.forEach((x, i) => {
+    nums[i] = x + 3;
+})
 
 //Find the average of all the numbers from question three
+let avg = nums.reduce((c, i) => c += i) / nums.length;
--- class-19/arrays-in-it-to-win-it/js/main.js
+++ class-19/arrays-in-it-to-win-it/js/main.js
@@ -1,7 +1,15 @@
 //Create an array of movies with at least three movies.
+let movies = [
+    'Bring It On',
+    'Bring It On Again',
+    'Not Another Teen Movie'
+];
 
 //Using the array from above, store the first movie in a variable
+let firstMovie = movies[0];
 
 //Get the length of the original array and store it in a new variable
+let moviesLen = movies.length();
 
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+let lastMovie = movies[movies.length - 1];
