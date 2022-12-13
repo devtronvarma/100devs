f00fdabcc23d8525b90f9d007f753e591087bb12
Dev Varma <devtronvarma@gmail.com>
2022-12-13 10:23:27 -0600

    complete review-winter-games exercise

--- class-20/review-winter-games/js/main.js
+++ class-20/review-winter-games/js/main.js
@@ -1,1 +1,12 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+function evens(arr) {
+  let res = [];
+
+  for (let i of arr) {
+    if (i % 2 === 0) {
+      res.push(i);
+    }
+  }
+
+  return res;
+}
