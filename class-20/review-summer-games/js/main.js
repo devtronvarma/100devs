70d3c7f8946180f3a93615bb245b7fc8414016af
Dev Varma <devtronvarma@gmail.com>
2022-12-13 09:54:48 -0600

    complete review-summer-games exercise

--- class-20/review-summer-games/js/main.js
+++ class-20/review-summer-games/js/main.js
@@ -1,1 +1,5 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers. Multiply each number together and alert the product.
+function arrProduct(arr) {
+  const res = arr.reduce((c, i) => c * i);
+  alert(res);
+}
