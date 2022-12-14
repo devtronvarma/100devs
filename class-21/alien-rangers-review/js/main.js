e2a4f4187ca907075c803c6760122c2dd5ba1c85
Dev Varma <devtronvarma@gmail.com>
2022-12-14 09:53:02 -0600

    complete alien-rangers array exercise

--- class-21/alien-rangers-review/js/main.js
+++ class-21/alien-rangers-review/js/main.js
@@ -1,9 +1,24 @@
 //Arrays
 
 //Create and array of tv shows. Loop through and print each show to the console
+let shows = [
+  "How I Met Your Mother",
+  "Friends",
+  "The Tom Green Show",
+  "Love is Blind",
+];
+
+shows.forEach((i) => console.log(i));
 
 //Create and array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
+let nums = [0, 4, 5, 6, 9];
+let evenNums = nums.filter((i) => i % 2 === 0);
+console.log(evenNums);
 
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+function sumLowestHighest(arr) {
+  let sorted = arr.sort((a, b) => a - b);
+  return sorted[0] + sorted[sorted.length - 1];
+}
