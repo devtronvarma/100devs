5faa3034f355fd4aa17a0d3066bb4b46e4582ba8
Dev Varma <devtronvarma@gmail.com>
2022-12-14 10:34:41 -0600

    complete objects exercise in class

--- class-21/objects-zeo/js/main.js
+++ class-21/objects-zeo/js/main.js
@@ -1,1 +1,16 @@
 //Create a stopwatch object that has four properties and three methods
+const stopwatch = {
+  currentTime: 12,
+  timeOfDay: "PM",
+  color: "black",
+  brand: "Nike",
+  start: function () {
+    console.log("STARTING...");
+  },
+  stop: function () {
+    console.log("STOPPPP!");
+  },
+  tellTime: function () {
+    console.log(`The current time is ${this.currentTime} ${this.timeOfDay}.`);
+  },
+};
