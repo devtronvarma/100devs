e2a3cc2417e28c2320ef0d2e7046e2c3bb40cdb1
Dev Varma <devtronvarma@gmail.com>
2022-12-15 09:41:42 -0600

    complete in-class arrays practice

--- class-22/space-review/js/main.js
+++ class-22/space-review/js/main.js
@@ -1,12 +1,31 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+const nums = [1, 2, 3, 4, 5];
+let sum = nums.reduce((a, c) => a + c);
+console.log(sum);
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+const arrSquared = (arr) => {
+  return arr.slice().map((i) => i ** 2);
+};
 
 //Create a function that takes string
 //Print the reverse of that string to the console
+const reverseString = (str) => {
+  const strArr = str.split("");
+  console.log(strArr.reverse().join(""));
+};
 
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+const isPalindrome = (s) => {
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
+    if (s[i] !== s[s.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
