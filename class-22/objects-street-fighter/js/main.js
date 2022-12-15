28e548293a05d9c7f96bb796fb12ce9834b559f3
Dev Varma <devtronvarma@gmail.com>
2022-12-15 10:43:47 -0600

    complete street-fighter constructor exercise

--- class-22/objects-street-fighter/js/main.js
+++ class-22/objects-street-fighter/js/main.js
@@ -1,1 +1,13 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function StreetFighter(name, side, gender, color) {
+  this.name = name;
+  this.side = side;
+  this.gender = gender;
+  this.color = color;
+  this.taunt = (message) => console.log(`${message}`);
+  this.die = () => console.log("You killed me.");
+  this.punch = () => console.log("Throw punch!");
+}
+
+let ryu = new StreetFighter("Ryu", "good", "male", "white");
+ryu.taunt("Haydukken!");
