8a48e6992bd0c075dd3a1f67608059497ee47ef8
Dev Varma <devtronvarma@gmail.com>
2022-12-15 10:18:20 -0600

    add example of object literal

--- class-22/objects-lost-galaxy/js/main.js
+++ class-22/objects-lost-galaxy/js/main.js
@@ -1,1 +1,13 @@
 //Create a mouse object that has four properties and three methods
+
+// Using object literal
+let mouse = {};
+
+mouse.brand = "Logitech";
+mouse.color = "black";
+mouse.model = "MX Ergo";
+mouse.wireless = true;
+
+mouse.leftClick = () => console.log("You clicked the left button.");
+mouse.rightClick = () => console.log("You clicked the right button.");
+mouse.scroll = (direction) => console.log(`You scrolled ${direction}.`);
