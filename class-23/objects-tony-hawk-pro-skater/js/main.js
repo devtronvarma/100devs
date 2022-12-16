3a58dd8ae896ae675c1255ec15bf387e8d4242d1
Dev Varma <devtronvarma@gmail.com>
2022-12-16 10:09:11 -0600

    practice class constructor syntax

--- class-23/objects-tony-hawk-pro-skater/js/main.js
+++ class-23/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,26 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function Skater(name, style, gender, favSong) {
+  this.name = name;
+  this.style = style;
+  this.gender = gender;
+  this.favSong = favSong;
+
+  this.doTrick = (trick) => {
+    console.log(`I'm doing a ${trick}`);
+  };
+
+  this.fallDown = () => {
+    console.log(`Ouch. that hurt.`);
+  };
+
+  this.playFavSong = () => {
+    console.log(`Oooh, ${this.favSong} is my jam.`);
+  };
+}
+
+let kareemCampbell = new Skater(
+  "Kareem",
+  "goofy",
+  "male",
+  "Dylan Dylan Dylan Dylan Dylan"
+);
