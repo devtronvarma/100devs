9c616ae28a16d9ab45a8e843c5f2cfe87c4a2976
Dev Varma <devtronvarma@gmail.com>
2022-12-16 09:58:02 -0600

    create dog object using object literal

--- class-23/objects-lost-galaxy/js/main.js
+++ class-23/objects-lost-galaxy/js/main.js
@@ -1,1 +1,11 @@
 //Create a dog object that has four properties and three methods
+const dog = {};
+
+dog.color = "black";
+dog.breed = "lab";
+dog.favToy = "cookie";
+dog.favFood = "cheese";
+
+dog.bark = () => console.log("Woof woof!");
+dog.jump = () => console.log("JUUUUMP");
+dog.run = () => console.log("Zoom!");
