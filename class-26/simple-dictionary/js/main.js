9ef7d51c9d3db3f95e87ec503d95dc38a173f32d
Dev Varma <devtronvarma@gmail.com>
2022-12-21 10:55:49 -0600

    build very simple dictionary api call

--- class-26/simple-dictionary/js/main.js
+++ class-26/simple-dictionary/js/main.js
@@ -1,16 +1,19 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
+function getFetch() {
   const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${choice}`
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data[0].meanings)
+      document.querySelector('h2').innerText = data[0].word
+      document.querySelector('#phonetic').innerText = data[0].phonetics[1].text
+      document.querySelector('#definitions').innerText = data[0].meanings[0].definitions[0].definition;
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    });
 }
