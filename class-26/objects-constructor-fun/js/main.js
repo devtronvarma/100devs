f9d82dbddb87fbc2119aeb9b24826faf4b3ce5ce
Dev Varma <devtronvarma@gmail.com>
2022-12-21 09:36:05 -0600

    practice object constructor syntax

--- class-26/objects-constructor-fun/js/main.js
+++ class-26/objects-constructor-fun/js/main.js
@@ -1,1 +1,10 @@
 //Create a constructor with 4 properties and 3 methods
+const Pizza = (toppings, crust, size, sauce) => {
+    this.toppings = toppings;
+    this.crust = crust;
+    this.size = size;
+    this.sauce = sauce;
+    this.burnMouth = () => console.log('Ouch!');
+    this.deliverPizza = () => console.log('Delivering your pizza!');
+    this.bakePizza = (temp) => console.log(`Baking your pizza at ${temp} degrees.`)
+}
