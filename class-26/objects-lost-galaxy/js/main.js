6934093ee794a08faa4e043a6476cbcbc1a2864a
Dev Varma <devtronvarma@gmail.com>
2022-12-21 09:01:19 -0600

    complete object literal exercise in class

--- class-26/objects-lost-galaxy/js/main.js
+++ class-26/objects-lost-galaxy/js/main.js
@@ -1,1 +1,15 @@
 //Create a dog object that has four properties and three methods
+const pizza = {}
+
+pizza.toppings = ['spinach', 'onions', 'banana peppers']
+pizza.sauce = 'hearty marinara'
+pizza.crust = 'hand-tossed'
+pizza.size = 'large'
+
+pizza.bake = function (temp) {
+    console.log(`Baking the pizza at ${temp} degrees.`)
+};
+
+pizza.eat = () => console.log(`That was yummy!`);
+
+pizza.burnMouth = () => console.log('AKJHDSLSKJHALJ!');
