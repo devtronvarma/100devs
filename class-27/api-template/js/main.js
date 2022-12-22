a6c967aa41d5292b58a10c96c4c5629dc59a2088
Dev Varma <devtronvarma@gmail.com>
2022-12-22 09:45:51 -0600

    added condition for nasa videos in media of the day

--- class-27/api-template/js/main.js
+++ class-27/api-template/js/main.js
@@ -8,7 +8,11 @@ function getFetch() {
   fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      document.querySelector('img').src = data.hdurl
+      if (data.media_type === 'image') {
+        document.querySelector('img').src = data.hdurl
+      } else if (data.media_type === 'video') {
+        document.querySelector('iframe').src = data.url
+      }
       document.querySelector('h3').innerText = data.explanation
     })
     .catch(err => {
