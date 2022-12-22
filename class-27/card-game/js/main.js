89d22b08d81b1514099b8427119d4315b24309ff
Dev Varma <devtronvarma@gmail.com>
2022-12-22 10:14:03 -0600

    init card game exercise

---
+++ class-27/card-game/js/main.js
@@ -0,0 +1,21 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch() {
+  const choice = document.querySelector('input').value
+  const url = `https://api.nasa.gov/planetary/apod?api_key=sRAntRrvaA7yrMq8GARS3xOXSVayCE008M5LkcAW&date=${choice}`
+
+  fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      if (data.media_type === 'image') {
+        document.querySelector('img').src = data.hdurl
+      } else if (data.media_type === 'video') {
+        document.querySelector('iframe').src = data.url
+      }
+      document.querySelector('h3').innerText = data.explanation
+    })
+    .catch(err => {
+      console.log(`error ${err}`)
+    });
+}
