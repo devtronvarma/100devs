806a28372f727611f212d63d05053318b04a922e
Dev Varma <devtronvarma@gmail.com>
2022-12-22 10:12:34 -0600

    practice working with setting and getting local storage

--- class-27/local-storage-botScore-button/js/main.js
+++ class-27/local-storage-botScore-button/js/main.js
@@ -1,3 +1,13 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+//Create a button that adds 1 to a botScore stored in localStorage
+if (!localStorage.getItem('botScore')) localStorage.setItem('botScore', 0);
+
+document.querySelector('button').addEventListener('click', addOne);
 
 
+
+function addOne() {
+    let botScoreVal = localStorage.getItem('botScore');
+    botScoreVal++;
+    localStorage.setItem('botScore', botScoreVal);
+    document.querySelector('h2').innerText = botScoreVal;
+}
