234addebae30829ebdb037532b7a60f071d09d62
Dev Varma <devtronvarma@gmail.com>
2022-12-22 09:35:52 -0600

    add nasa api connection and form to fetch pic of the day

--- class-27/api-template/js/main.js
+++ class-27/api-template/js/main.js
@@ -3,12 +3,13 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch() {
   const choice = document.querySelector('input').value
-  const url = `https://pokeapi.co/api/v2/pokemon/${choice}`
+  const url = `https://api.nasa.gov/planetary/apod?api_key=sRAntRrvaA7yrMq8GARS3xOXSVayCE008M5LkcAW&date=${choice}`
 
   fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data)
+      document.querySelector('img').src = data.hdurl
+      document.querySelector('h3').innerText = data.explanation
     })
     .catch(err => {
       console.log(`error ${err}`)
